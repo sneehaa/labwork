44383cfb92eb9f72ff3f2b105cf99d0edb19067b
sneehaa <adhikarisneha0001@gmail.com>
2021-12-05 11:16:20 +0545

    added folder

---
+++ day2/lab1.py
@@ -0,0 +1,7 @@
+x= 86400
+day= (x/(24*60*60))
+hour= (x/(60*60))
+minute=(x/60)
+print(day)
+print(hour)
+print(minute)
---
+++ day2/lab2.py
@@ -0,0 +1,4 @@
+h= int(input("heightin m: "))
+a= int(input("mass in kg: "))
+BMI= a/h*2
+print("the BMI of a person is {} kg/m\u00b2". format(BMI))
